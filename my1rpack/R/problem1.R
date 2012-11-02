5c0620120cbd3a14b8c7229dc2cf495f08f3107c
GraceYueWu <graceyue523@gmail.com>
2012-11-02 01:26:58 -0400

    all work done

--- my1rpack/R/problem1.R
+++ my1rpack/R/problem1.R
@@ -1,16 +1,21 @@
 problem1 <- function(filename=NULL,
-                     gcnname=NULL,
+                     fcnname=NULL,
                      range=NULL,
                      nsample=NULL,
                      compile=NULL){
   outputtexfile = paste(filename, '.tex', sep ='')
   require(tikzDevice);
   tikz(file=outputtexfile,standAlone=T);
+  #tikz(outputtexfile,standAlone=TRUE)
   xbeg = range[1]
   xend = range[2]
-  xval = seq(xbeg, xend, length=nsample)
-  yval = gcnname(xval)
+  xval = runif(nsample, xbeg, xend)
+  xval = sort(xval, decreasing=F)
+  yval = fcnname(xval)
   plot(xval, yval, type = 'l', main = '',  xlab = '\\verb+x+', ylab = '\\verb+dnorm(x)+')
-  dev.off() 
-  tools::texi2pdf(outputtexfile)
+  dev.off()
+  if(compile){
+    tools::texi2pdf(outputtexfile)
+  }
+  #plot(xval,yval,type='l')
 }
