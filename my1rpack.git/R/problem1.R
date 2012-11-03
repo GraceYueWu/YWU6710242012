1f2605f3ff0930c3f6fddbea6bf8d096a7139390
GraceYueWu <graceyue523@gmail.com>
2012-11-02 21:53:32 -0400

    warnings fixed in the package

--- my1rpack.git/R/problem1.R
+++ my1rpack.git/R/problem1.R
@@ -1,15 +1,15 @@
 problem1 <- function(filename=NULL,
-                     gcnname=NULL,
-                     range=NULL,
+                     fcnname=NULL,
+                     nrange=NULL,
                      nsample=NULL,
                      compile=NULL){
   outputtexfile = paste(filename, '.tex', sep ='')
-  require(tikzDevice);
+  library(tikzDevice);
   tikz(file=outputtexfile,standAlone=T);
-  xbeg = range[1]
-  xend = range[2]
+  xbeg = nrange[1]
+  xend = nrange[2]
   xval = seq(xbeg, xend, length=nsample)
-  yval = gcnname(xval)
+  yval = fcnname(xval)
   plot(xval, yval, type = 'l', main = '',  xlab = '\\verb+x+', ylab = '\\verb+dnorm(x)+')
   dev.off() 
   tools::texi2pdf(outputtexfile)
